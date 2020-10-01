04c9a8948ba5a6ec8101673e8cc4977f9eddccb9
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-10-01 19:09:16 +0300

    added getCart method (handled hibernate lazy initializer using DTOs)

---
+++ src/main/java/org/fasttrackit/onlineshop/transfer/cart/CartResponse.java
@@ -0,0 +1,22 @@
+package org.fasttrackit.onlineshop.transfer.cart;
+
+public class CartResponse {
+
+    private long id;
+    // product details to be added later
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "CartResponse{" +
+                "id=" + id +
+                '}';
+    }
+}
