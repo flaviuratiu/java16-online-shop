9fc50c3ba70b0512b3b06cfbbbdeea0909d7d1e7
Flaviu Ratiu <flaviuratiu@gmail.com>
2020-10-08 19:11:30 +0300

    finished implementing addProductToCart test

--- src/test/java/org/fasttrackit/onlineshop/cart/CartServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshop/cart/CartServiceIntegrationTests.java
@@ -2,10 +2,12 @@ package org.fasttrackit.onlineshop.cart;
 
 import org.fasttrackit.onlineshop.domain.User;
 import org.fasttrackit.onlineshop.service.CartService;
-import org.fasttrackit.onlineshop.service.UserService;
+import org.fasttrackit.onlineshop.steps.ProductTestSteps;
 import org.fasttrackit.onlineshop.steps.UserTestSteps;
 import org.fasttrackit.onlineshop.transfer.cart.AddProductToCartRequest;
 import org.fasttrackit.onlineshop.transfer.cart.CartResponse;
+import org.fasttrackit.onlineshop.transfer.cart.ProductInCart;
+import org.fasttrackit.onlineshop.transfer.product.ProductResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 @SpringBootTest
 public class CartServiceIntegrationTests {
@@ -23,13 +26,17 @@ public class CartServiceIntegrationTests {
     @Autowired
     private UserTestSteps userTestSteps;
 
+    @Autowired
+    private ProductTestSteps productTestSteps;
+
     @Test
     public void addProductToCart_whenValidRequest_thenProductsAreAddedToCart() {
         User user = userTestSteps.createUser();
+        ProductResponse product = productTestSteps.createProduct();
 
         AddProductToCartRequest request = new AddProductToCartRequest();
         request.setUserId(user.getId());
-        request.setProductId(1L);
+        request.setProductId(product.getId());
 
         cartService.addProductToCart(request);
 
@@ -37,5 +44,16 @@ public class CartServiceIntegrationTests {
 
         assertThat(cart, notNullValue());
         assertThat(cart.getId(), is(request.getUserId()));
+
+        assertThat(cart.getProducts(), notNullValue());
+        assertThat(cart.getProducts(), hasSize(1));
+
+        ProductInCart productInCart = cart.getProducts().iterator().next();
+
+        assertThat(productInCart, notNullValue());
+        assertThat(productInCart.getId(), is(product.getId()));
+        assertThat(productInCart.getName(), is(product.getName()));
+        assertThat(productInCart.getPrice(), is(product.getPrice()));
+        assertThat(productInCart.getImageUrl(), is(product.getImageUrl()));
     }
 }
